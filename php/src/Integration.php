90e59363f89ed1bd0ed40685a6aca95b0293bbfb
Michael Holloway <mholloway@wikimedia.org>
2021-07-19 13:53:21 -0400

    [PHP] Support adding context values based on stream config
    Bug: T274175
    Bug: T281752
    Change-Id: I4dcedbb0667ac9c9e5695d1559cc9706f6f2636d

--- php/src/Integration.php
+++ php/src/Integration.php
@@ -36,4 +36,136 @@ interface Integration {
 	 */
 	public function send( array $event ) : void;
 
+	// Context
+
+	/**
+	 * @return int|null
+	 */
+	public function getPageId() : ?int;
+
+	/**
+	 * @return int|null
+	 */
+	public function getPageNamespaceId() : ?int;
+
+	/**
+	 * @return string|null
+	 */
+	public function getPageNamespaceText() : ?string;
+
+	/**
+	 * @return string|null
+	 */
+	public function getPageTitle() : ?string;
+
+	/**
+	 * @return bool|null
+	 */
+	public function getPageIsRedirect() : ?bool;
+
+	/**
+	 * @return int|null
+	 */
+	public function getPageRevisionId() : ?int;
+
+	/**
+	 * @return string|null
+	 */
+	public function getPageWikidataItemId() : ?string;
+
+	/**
+	 * @return string|null
+	 */
+	public function getPageContentLanguage() : ?string;
+
+	/**
+	 * @return array|null
+	 */
+	public function getPageGroupsAllowedToEdit() : ?array;
+
+	/**
+	 * @return array|null
+	 */
+	public function getPageGroupsAllowedToMove() : ?array;
+
+	/**
+	 * @return int|null
+	 */
+	public function getUserId() : ?int;
+
+	/**
+	 * @return bool|null
+	 */
+	public function getUserIsLoggedIn() : ?bool;
+
+	/**
+	 * @return bool|null
+	 */
+	public function getUserIsBot() : ?bool;
+
+	/**
+	 * @return string|null
+	 */
+	public function getUserName() : ?string;
+
+	/**
+	 * @return array|null
+	 */
+	public function getUserGroups() : ?array;
+
+	/**
+	 * @return bool|null
+	 */
+	public function getUserCanProbablyEditPage() : ?bool;
+
+	/**
+	 * @return int|null
+	 */
+	public function getUserEditCount() : ?int;
+
+	/**
+	 * @return string|null
+	 */
+	public function getUserEditCountBucket() : ?string;
+
+	/**
+	 * @return int|null
+	 */
+	public function getUserRegistrationTimestamp() : ?int;
+
+	/**
+	 * @return string|null
+	 */
+	public function getUserLanguage() : ?string;
+
+	/**
+	 * @return string|null
+	 */
+	public function getUserLanguageVariant() : ?string;
+
+	/**
+	 * @return string|null
+	 */
+	public function getMediaWikiSkin() : ?string;
+
+	/**
+	 * @return string|null
+	 */
+	public function getMediaWikiVersion() : ?string;
+
+	/**
+	 * @return string|null
+	 */
+	public function getMediaWikiSiteContentLanguage() : ?string;
+
+	/**
+	 * @return string|null
+	 */
+	public function getAccessMethod() : ?string;
+
+	/**
+	 * @return bool|null
+	 */
+	public function isProduction() : ?bool;
+
 }
--- php/tests/TestIntegration.php
+++ php/tests/TestIntegration.php
@@ -25,4 +25,136 @@ class TestIntegration implements Integration {
 	public function send( array $event ) : void {
 	}
 
+	// Context
+
+	/** @inheritDoc */
+	public function getPageId(): ?int {
+		return 1;
+	}
+
+	/** @inheritDoc */
+	public function getPageNamespaceId(): ?int {
+		return 0;
+	}
+
+	/** @inheritDoc */
+	public function getPageNamespaceText(): ?string {
+		return "";
+	}
+
+	/** @inheritDoc */
+	public function getPageTitle(): ?string {
+		return "Test";
+	}
+
+	/** @inheritDoc */
+	public function getPageIsRedirect(): ?bool {
+		return false;
+	}
+
+	/** @inheritDoc */
+	public function getPageRevisionId(): ?int {
+		return 1;
+	}
+
+	/** @inheritDoc */
+	public function getPageWikidataItemId(): ?string {
+		return "Q1";
+	}
+
+	/** @inheritDoc */
+	public function getPageContentLanguage(): ?string {
+		return "zh";
+	}
+
+	/** @inheritDoc */
+	public function getPageGroupsAllowedToEdit(): ?array {
+		return [];
+	}
+
+	/** @inheritDoc */
+	public function getPageGroupsAllowedToMove(): ?array {
+		return [];
+	}
+
+	/** @inheritDoc */
+	public function getUserId(): ?int {
+		return 1;
+	}
+
+	/** @inheritDoc */
+	public function getUserIsLoggedIn(): ?bool {
+		return true;
+	}
+
+	/** @inheritDoc */
+	public function getUserIsBot(): ?bool {
+		return false;
+	}
+
+	/** @inheritDoc */
+	public function getUserName(): ?string {
+		return "TestUser";
+	}
+
+	/** @inheritDoc */
+	public function getUserGroups(): ?array {
+		return [ "*" ];
+	}
+
+	/** @inheritDoc */
+	public function getUserCanProbablyEditPage(): ?bool {
+		return true;
+	}
+
+	/** @inheritDoc */
+	public function getUserEditCount(): ?int {
+		return 10;
+	}
+
+	/** @inheritDoc */
+	public function getUserEditCountBucket(): ?string {
+		return "5-99 edits";
+	}
+
+	/** @inheritDoc */
+	public function getUserRegistrationTimestamp(): ?int {
+		return 1427224089000;
+	}
+
+	/** @inheritDoc */
+	public function getUserLanguage(): ?string {
+		return "zh";
+	}
+
+	/** @inheritDoc */
+	public function getUserLanguageVariant(): ?string {
+		return "zh-tw";
+	}
+
+	/** @inheritDoc */
+	public function getMediaWikiSkin(): ?string {
+		return "timeless";
+	}
+
+	/** @inheritDoc */
+	public function getMediaWikiVersion(): ?string {
+		return "1.37.0";
+	}
+
+	/** @inheritDoc */
+	public function getMediaWikiSiteContentLanguage(): ?string {
+		return "zh";
+	}
+
+	/** @inheritDoc */
+	public function getAccessMethod(): ?string {
+		return "mobile web";
+	}
+
+	/** @inheritDoc */
+	public function isProduction(): ?bool {
+		return true;
+	}
+
 }
