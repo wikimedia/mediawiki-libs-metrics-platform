ad6d2153204ca97d8281ea46e21202038e71f9ee
Sam Smith <phuedx@wikimedia.org>
2023-01-17 11:54:00 +0000

    [Swift] Refactor SamplingController
    * Restrict the API to SamplingController.inSample()
    
    * Rename SamplingConfig.identifier to SamplingConfig.unit to match
      production stream configs and update SamplingController accordingly
    
    Bug: T281768
    Change-Id: If2ad506395cdb1f8085ce639fa9d1339f316d6e5

--- swift/Sources/WikimediaMetricsPlatform/SamplingController.swift
+++ swift/Sources/WikimediaMetricsPlatform/SamplingController.swift
@@ -5,84 +5,57 @@ class SamplingController {
     private let integration: MetricsClientIntegration
     private let sessionController: SessionController
 
-    /**
-     * Cache of "in sample" / "out of sample" determination for each stream
-     *
-     * The process of determining only has to happen the first time an event is
-     * logged to a stream for which stream configuration is available. All other
-     * times `in_sample` simply returns the cached determination.
-     *
-     * Only cache determinations asynchronously via `queue.async`
-     */
-    private var samplingCache: [String: Bool] = [:]
-
     init(integration: MetricsClientIntegration, sessionController: SessionController) {
         self.integration = integration
         self.sessionController = sessionController
     }
 
     /**
-     * Compute a boolean function on a random identifier
-     * - Parameter stream: name of the stream
-     * - Parameter config: stream configuration for the provided stream name
-     * - Returns: `true` if in sample or `false` otherwise
+     * Gets whether the given stream is in-sample.
      *
-     * The determinations are lazy and cached, so each stream's in-sample vs
-     * out-of-sample determination is computed only once, the first time an event
-     * is logged to that stream.ß
+     * If the stream is not configured for sampling, then it is always in-sample.
      *
-     * Refer to sampling settings section in
-     * [mw:Wikimedia Product/Analytics Infrastructure/Stream configuration](https://www.mediawiki.org/wiki/Wikimedia_Product/Analytics_Infrastructure/Stream_configuration)
+     * If the stream is incorrectly configured for sampling – the sampling unit is not either "session" or "device", or
+     * the sampling rate is not between 0 and 1 – then the stream is always out-sample.
+     *
+     * Otherwise, perform a deterministic (not stochastic) determination of whether the configured unit is in-sample
+     * according to the configured rate.
+     *
+     * The algorithm works in a "widen the net on frozen fish" fashion – a stream determined to be in-sample for a
+     * lower rate will be determined to be in-sample for a higher rate. For example: two streams, A and B, are both
+     * configured to sample based on the device identifier at a rate of 0.1 and 0.2, respectively; events logged to
+     * stream A could also be logged to stream B.
+     *
+     * Refer to sampling settings section in [mw:Wikimedia Product/Analytics Infrastructure/Stream configuration][0]
      * for more information.
+     *
+     * [0]:  https://www.mediawiki.org/wiki/Wikimedia_Product/Analytics_Infrastructure/Stream_configuration
      */
     func inSample(stream: String, config: StreamConfig) -> Bool {
-        guard let rate = config.getSamplingConfig()?.rate else {
-            /**
-             * If stream is present in streamConfigs but doesn't have
-             * sampling settings, it is always in-sample.
-             */
+        guard let samplingConfig = config.getSamplingConfig() else {
+            /// If the stream config does not have a sampling config, then the stream is always in-sample.
             return true
         }
 
-        /**
-         * All platforms use session ID as the default identifier for determining
-         * in- vs out-of-sample of events sent to streams. On the web, streams can
-         * be set to use pageview token instead. On the apps, streams can be set
-         * to use device token instead.
-         */
-        let sessionIdentifierType = "session"
-        let deviceIdentifierType = "device"
-        let identifierType = config.getSamplingConfig()?.identifier ?? sessionIdentifierType
-        let appInstallID = integration.appInstallId()
+        guard
+            let unit = samplingConfig.unit,
+            unit == "session" || unit == "device",
+            let rate = samplingConfig.rate
+        else {
+            NSLog(
+                "MetricsClient: Stream \"\(stream)\" cannot be determined to be in-sample. The sampling unit must be either \"session\" or \"device\" and the rate must be a number between 0 and 1."
+            )
 
-        guard (identifierType == sessionIdentifierType) || (identifierType == deviceIdentifierType) else {
-            NSLog("MetricsClient: Logged to stream which is not configured for sampling based on \(sessionIdentifierType) or \(deviceIdentifierType) identifier")
             return false
         }
-
-        let identifier = identifierType == sessionIdentifierType ? sessionController.sessionId() : appInstallID
-        return determine(identifier, rate)
-    }
-
-    /**
-     * Yields a deterministic (not stochastic) determination of whether the
-     * provided `id` is in-sample or out-of-sample according to the `acceptance`
-     * rate
-     * - Parameter id: identifier to use for determining sampling
-     * - Parameter acceptance: the desired proportion of many `token`-s being
-     *   accepted
-     *
-     * The algorithm works in a "widen the net on frozen fish" fashion -- tokens
-     * continue evaluating to true as the acceptance rate increases. For example,
-     * a device determined to be in-sample for a stream "A" having rate 0.1 will
-     * be determined to be in-sample for a stream "B" having rate 0.2, and its
-     * events will show up in tables "A" and "B".
-     */
-    func determine(_ id: String, _ acceptance: Double) -> Bool {
+        
+        let id = unit == "session" ? sessionController.sessionId() : integration.appInstallId();
+ 
         guard let token = UInt32(id.prefix(8), radix: 16) else {
+            /// NOTE: This should never happen.
             return false
         }
-        return (Double(token) / Double(UInt32.max)) < acceptance
-    }
 
+        return (Double(token) / Double(UInt32.max)) < rate
+    }
 }
