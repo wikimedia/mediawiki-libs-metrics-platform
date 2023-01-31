2fba928c9834fea09dbac659faba47e90a052ddc
Clare Ming <cming@wikimedia.org>
2023-01-30 17:48:42 -0700

    Add tests for AgentData, MediawikiData classes
    Bug: T324996
    Change-Id: Ib36d1a99c244e5eebafdf146fd3e19556073f11e

---
+++ java/src/test/java/org/wikimedia/metrics_platform/context/AgentDataTest.java
@@ -0,0 +1,30 @@
+package org.wikimedia.metrics_platform.context;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import com.google.gson.Gson;
+
+public class AgentDataTest {
+    @Test
+    void testAgentData() {
+        AgentData agentData = AgentData.builder()
+                .appInstallId("ffffffff-ffff-ffff-ffff-ffffffffffff")
+                .clientPlatform("android")
+                .clientPlatformFamily("app")
+                .build();
+
+        assertThat(agentData.getAppInstallId()).isEqualTo("ffffffff-ffff-ffff-ffff-ffffffffffff");
+        assertThat(agentData.getClientPlatform()).isEqualTo("android");
+        assertThat(agentData.getClientPlatformFamily()).isEqualTo("app");
+
+        Gson gson = new Gson();
+        String json = gson.toJson(agentData);
+        assertThat(json).isEqualTo("{" +
+                "\"app_install_id\":\"ffffffff-ffff-ffff-ffff-ffffffffffff\"," +
+                "\"client_platform\":\"android\"," +
+                "\"client_platform_family\":\"app\"" +
+                "}");
+    }
+}
---
+++ java/src/test/java/org/wikimedia/metrics_platform/context/MediawikiDataTest.java
@@ -0,0 +1,42 @@
+package org.wikimedia.metrics_platform.context;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import com.google.gson.Gson;
+
+public class MediawikiDataTest {
+    @Test
+    void testMediawikiData() {
+        MediawikiData mediawikiData = MediawikiData.builder()
+                .skin("vector")
+                .version("1.40.0-wmf.20")
+                .isProduction(true)
+                .isDebugMode(false)
+                .database("enwiki")
+                .siteContentLanguage("en")
+                .siteContentLanguageVariant("en-zh")
+                .build();
+
+        assertThat(mediawikiData.getSkin()).isEqualTo("vector");
+        assertThat(mediawikiData.getVersion()).isEqualTo("1.40.0-wmf.20");
+        assertThat(mediawikiData.getIsProduction()).isTrue();
+        assertThat(mediawikiData.getIsDebugMode()).isFalse();
+        assertThat(mediawikiData.getDatabase()).isEqualTo("enwiki");
+        assertThat(mediawikiData.getSiteContentLanguage()).isEqualTo("en");
+        assertThat(mediawikiData.getSiteContentLanguageVariant()).isEqualTo("en-zh");
+
+        Gson gson = new Gson();
+        String json = gson.toJson(mediawikiData);
+        assertThat(json).isEqualTo("{" +
+                "\"skin\":\"vector\"," +
+                "\"version\":\"1.40.0-wmf.20\"," +
+                "\"is_production\":true," +
+                "\"is_debug_mode\":false," +
+                "\"database\":\"enwiki\"," +
+                "\"site_content_language\":\"en\"," +
+                "\"site_content_language_variant\":\"en-zh\"" +
+                "}");
+    }
+}
