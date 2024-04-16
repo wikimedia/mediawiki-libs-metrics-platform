41ce3145685bf4db1ae5a590a5b79e459982c42d
Phuedx <phuedx@wikimedia.org>
2024-04-16 17:31:06 +0000

    [PHP] Update MetricsClient#submitInteraction()

--- php/src/MetricsClient.php
+++ php/src/MetricsClient.php
@@ -6,6 +6,7 @@ use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
+use Wikimedia\MetricsPlatform\StreamConfig\StreamConfigException;
 use Wikimedia\MetricsPlatform\StreamConfig\StreamConfigFactory;
 use Wikimedia\Timestamp\ConvertibleTimestamp;
 
@@ -96,6 +97,15 @@ class MetricsClient implements LoggerAwareInterface {
 		$event = $this->createEvent( $action, $schemaId );
 		$formattedInteractionData = $this->getInteractionData( $action, $interactionData );
 		$eventData = array_merge( $event, $formattedInteractionData );
+
+		try {
+			$streamConfig = $this->streamConfigFactory->getStreamConfig( $streamName );
+
+			$eventData = $this->contextController->addRequestedValues( $eventData, $streamConfig );
+		} catch ( StreamConfigException $e ) {
+			return;
+		}
+
 		$this->eventSubmitter->submit( $streamName, $eventData );
 	}
 
