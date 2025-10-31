6dc3147c5b29406d7d1963585badf05f25eddc4e
Sanjai Siddharthan <sanjaisiddharth2002@gmail.com>
2025-10-31 23:07:57 +0530

    Add testcase for SamplingController
    Bug: T355055

--- java/src/test/java/org/wikimedia/metrics_platform/SamplingControllerTest.java
+++ java/src/test/java/org/wikimedia/metrics_platform/SamplingControllerTest.java
@@ -7,6 +7,8 @@ import static org.wikimedia.metrics_platform.config.sampling.SampleConfig.Identi
 import org.junit.jupiter.api.Test;
 import org.wikimedia.metrics_platform.config.sampling.SampleConfig;
 import org.wikimedia.metrics_platform.config.StreamConfig;
+import org.wikimedia.metrics_platform.context.AgentData;
+import org.wikimedia.metrics_platform.context.ClientData;
 import org.wikimedia.metrics_platform.context.DataFixtures;
 
 class SamplingControllerTest {
@@ -53,6 +55,61 @@ class SamplingControllerTest {
                 new SampleConfig(0.0, SESSION)
         );
         assertThat(samplingController.isInSample(neverInSample)).isFalse();
+    }
+
+    @Test void testSamplingMonotonicity() {
+        String deviceIdInSampleAt1Percent = findDeviceIdInSampleAtRate(0.01);
+        assertThat(deviceIdInSampleAt1Percent).isNotNull();
+
+        ClientData clientData = new ClientData(
+                AgentData.builder()
+                        .appInstallId(deviceIdInSampleAt1Percent)
+                        .clientPlatform("android")
+                        .clientPlatformFamily("app")
+                        .build(),
+                DataFixtures.getTestPageData(),
+                DataFixtures.getTestMediawikiData(),
+                DataFixtures.getTestPerformerData(),
+                "en.wikipedia.org"
+        );
+        SamplingController controller = new SamplingController(clientData, new SessionController());
+
+        double[] testRates = {0.01, 0.05, 0.10, 0.25, 0.50, 0.75, 1.0};
+        for (double rate : testRates) {
+            StreamConfig streamConfig = new StreamConfig("foo", "bar", null,
+                    new StreamConfig.ProducerConfig(new StreamConfig.MetricsPlatformClientConfig(
+                            null,
+                            null,
+                            null
+                    )),
+                    new SampleConfig(rate, DEVICE)
+            );
+            assertThat(controller.isInSample(streamConfig))
+                    .withFailMessage("Device should be in-sample at rate " + (rate * 100) + "%")
+                    .isTrue();
+        }
+    }
+
+    private String findDeviceIdInSampleAtRate(double rate) {
+        for (int i = 0; i < 10000; i++) {
+            String deviceId = String.format(java.util.Locale.ROOT, "%08x-0000-0000-0000-000000000000", i);
+            ClientData testClientData = new ClientData(
+                    AgentData.builder()
+                            .appInstallId(deviceId)
+                            .clientPlatform("android")
+                            .clientPlatformFamily("app")
+                            .build(),
+                    DataFixtures.getTestPageData(),
+                    DataFixtures.getTestMediawikiData(),
+                    DataFixtures.getTestPerformerData(),
+                    "en.wikipedia.org"
+            );
+            SamplingController testController = new SamplingController(testClientData, new SessionController());
+            if (testController.getSamplingValue(DEVICE) < rate) {
+                return deviceId;
+            }
+        }
+        return null;
     }
 
 }
